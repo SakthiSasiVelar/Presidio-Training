6d4716923dfeb6778c40f418ed4b7b1f81926127
Sakthi sasi velar <sakthisasivelar@gmail.com>
2024-04-15 17:20:19 +0530

    day 5 commit

---
+++ Day 5/Solution CowBullGame app/CowBullGame app/BullCowGame.cs
@@ -0,0 +1,62 @@
+﻿namespace CowBullGame_app
+{
+    internal class BullCowGame
+    {
+        static string GetWord()
+        {
+            return Console.ReadLine();
+        }
+
+        static int CheckSameCharacterAndPosition(string word , char characterToFind , int indexOfCharacter) {
+            for(int i = 0;i < word.Length;i++)
+            {
+                if (word[i] == characterToFind)
+                {
+                    if (indexOfCharacter == i) return 1;
+                    return 2;
+                }
+            }
+            return 0;
+        }
+
+        static void PrintValueOfCowsAndBulls(int cows , int bulls )
+        {
+            Console.WriteLine($"cows - {cows} , bulls - {bulls}");
+        }
+
+        static void PrintCongratsMessage()
+        {
+            Console.WriteLine("Congrats!!!you won!!!");
+        }
+        static bool IsWordMatches(string wordToBeFind , string guessWord)
+        {
+            int cows = 0, bulls = 0;
+            for(int i = 0; i < guessWord.Length; i++)
+            {
+                int result = CheckSameCharacterAndPosition(wordToBeFind, guessWord[i] , i) ;
+                if (result == 1) cows++;
+                else if (result == 2) bulls++;
+            }
+            PrintValueOfCowsAndBulls(cows, bulls);
+            if(cows == 4)
+            {
+                PrintCongratsMessage();
+                return true;
+            }
+            return false;
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter four letter word to find :");
+            string wordToBeFind = GetWord();
+            Console.WriteLine("Start the guess : ");
+            string guessWord = GetWord();
+            while(!IsWordMatches(wordToBeFind, guessWord))
+            {
+                guessWord = GetWord();
+            }
+
+        }
+    }
+}
---
+++ Day 5/solution Task 13/Task 13/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_13
+{
+    internal class Program
+    {
+        static int[] GetArrayInput(int n)
+        {
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter three digit number");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return numbers;
+        }
+
+        static int getRepeatingThreeDigitNumberCount(int[] numbers , int sizeOfArray)
+        {
+            int countRepeatingThreeDigitNumber = 0;
+            for(int i = 0; i < sizeOfArray; i++)
+            {
+                string number = numbers[i].ToString();
+                char req_digit = number[0];
+                int j;
+                for (j=0; j < number.Length; j++)
+                {
+                    if (number[j] != req_digit) break;
+                }
+                 if(j == number.Length) countRepeatingThreeDigitNumber++;
+            }
+            return countRepeatingThreeDigitNumber;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of an array");
+            int sizeOfAnArray = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = GetArrayInput(sizeOfAnArray);
+            int countOfRepeatingThreeDigitNumber = getRepeatingThreeDigitNumberCount(numbers , sizeOfAnArray);
+            Console.WriteLine("The count of repeating three digit number : " +  countOfRepeatingThreeDigitNumber);
+
+        }
+    }
+}
